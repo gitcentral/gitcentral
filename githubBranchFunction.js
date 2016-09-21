ca3e8ba8013f4e962ed60806c850470ee71ced86
Martin <martinkkwan@gmail.com>
2016-09-21 16:44:07 -0700

    (feat) add function to name orphan branches

--- githubBranchFunction.js
+++ githubBranchFunction.js
@@ -1,7 +1,9 @@
 
 // let {JSONcommits, SHALookup, JSONbranches} = require ('./a.js');
 
+// let {JSONcommits, JSONbranches} = require ('./testJSON.js');
 let {JSONcommits, JSONbranches} = require ('./jsonTogitGraph.js');
+
 let SHALookup = { "sha" : {}};
 /**
  * Set up SHALookup hash table
@@ -51,10 +53,15 @@ JSONbranches.filter((branch)=>{
   checkBranch(commit);
 });
 
+/**
+ * Recurse through each commit's parents and pass on its branch name
+ * @param  {[type]} name   [name property of branch]
+ * @param  {[type]} sha [sha property of branch]
+ * @return {[type]}        [description]
+ */
 function checkBranch({name,commit:{sha}}){
   const commit = SHALookup[sha];
   const checkBranchName = (commitObj)=>{
-    // console.log(commitObj.branch);
     if(commitObj===undefined){
       return;
     }
@@ -67,10 +74,86 @@ function checkBranch({name,commit:{sha}}){
   checkBranchName(commit);
 }
 
+// const checkOrphan = (commitObj)=>{
+//   if(commitObj===undefined){
+//     return;
+//   }
+//   if(commitObj.children > 1){
+//     return;
+//   }
+//   console.log(commitObj.children,"??????");
+//   commitObj.branch = SHALookup[commitObj.children[0]].branch;
+//   checkOrphan(SHALookup[commitObj.parents[0].sha]);
+// }
+
+// const makeOrphan = (commitObj)=>{
+//
+//   let branchName = commitObj.branch + commitObj.sha.slice(0,3);
+//
+//
+//   commitObj.branch = branchName;
+//
+//   checkOrphan(SHALookup[commitObj.parents[0].sha]);
+//
+//
+// }
+//
+// const checkMerge = (commitObj) => {
+//   //check if current branch is not equal to incoming branch or else break
+//   if(commitObj===undefined){
+//     return;
+//   }
+//   if(commitObj.parents===undefined){
+//     return;
+//   }
+//   // console.log(commitObj.parents);
+//   if(commitObj.parents[1]){
+//     makeOrphan(SHALookup[commitObj.parents[1].sha]);
+//   }
+//   checkMerge(SHALookup[commitObj.parents[0].sha]);
+//
+// }
+
+const filter2Parents = (JSONcommitObj)=>{
+  return JSONcommitObj.parents.length > 1;
+}
+
+const getRightParent = (JSONcommitObj)=>{
+  return SHALookup[JSONcommitObj.parents[1].sha];
+}
+
+const renameOrphanParent = (JSONcommitObj)=>{
+  JSONcommitObj.branch += JSONcommitObj.sha.slice(0,5);
+
+  const checkOrphan = (commitObj,branchName)=>{
+    if(commitObj===undefined){
+      return;
+    }
+    if(commitObj.children.length > 1){
+      return;
+    }
+    commitObj.branch = branchName;
+    if(commitObj.parents.length > 0){
+      checkOrphan(SHALookup[commitObj.parents[0].sha],branchName);
+    }
+  }
+  let leftParent = SHALookup[JSONcommitObj.parents[0].sha];
+  checkOrphan(leftParent,JSONcommitObj.branch);
+}
+
+JSONcommits.filter(filter2Parents).map(getRightParent).map(renameOrphanParent);
+
+JSONbranches.forEach((branch)=>{
+  const commit = SHALookup[branch.commit.sha];
+
+  // console.log(commit);
+  // checkMerge(commit);
+});
+
 
 
 console.log("after-=============branches");
 JSONcommits.forEach(function(commit) {
-  console.log({ sha: commit.sha, branch:commit.branch});
+  console.log({ sha: commit.sha.slice(0,5), branch:commit.branch});
 
 })
--- jsonTogitGraph.js
+++ jsonTogitGraph.js
@@ -1,3 +1,20 @@
+var JSONbranches = [
+  {
+    "name": "dev",
+    "commit": {
+      "sha": "453e2c9462895b8b310eaf729821180b1ef4f60b",
+      "url": "https://api.github.com/repos/mangonada/mangonada/commits/453e2c9462895b8b310eaf729821180b1ef4f60b"
+    }
+  },
+  {
+    "name": "master",
+    "commit": {
+      "sha": "56d20161b7aae27c3e5632a4ea6a7e73db1457ed",
+      "url": "https://api.github.com/repos/mangonada/mangonada/commits/56d20161b7aae27c3e5632a4ea6a7e73db1457ed"
+    }
+  }
+];
+
 var JSONcommits = [
     {
         "sha": "453e2c9462895b8b310eaf729821180b1ef4f60b",
@@ -1209,3 +1226,5 @@ var JSONcommits = [
         "parents": []
     }
 ]
+
+module.exports = {JSONcommits, JSONbranches};
