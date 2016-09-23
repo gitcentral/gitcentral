c9498b2407d3277d45caf0fe8403416314e4ed08
Timothy Shiu <timoweave@gmail.com>
2016-09-23 16:35:32 -0700

    (feat) parse json data from github api

--- api.js
+++ api.js
@@ -30,23 +30,46 @@ router.route('/repos/:userName/:repoName')
   let secrets = "client_id=423335fdf206466ccd3b&client_secret=bc10a999efc0335d06b6d84d470b76eda5a97b30";
   let branches = [];
 
+  console.log(1);
   const branchOpt = { uri : `${branchesURL}?${secrets}`, headers : { 'User-Agent' : userAgent } };
   request(branchOpt)
   .then(function (branches) {
+    console.log(11);
     branches = JSON.parse(branches);
     var requestBranchCommits = branches.reduce(makeRequestBranchCommits, []);
-
-    Promise.all(requestBranchCommits).then(allCommits => { 
-      allCommits.map((branchCommits) => {
-        const branchCommitArray = JSON.parse(branchCommits);
+    var requestAllCommits = { accum : [], lookup : {} };
+    console.log(111);
+    Promise.all(requestBranchCommits).then(allCommits => {
+      console.log(1111);
+      const allFlattenCommits = allCommits.reduce( (r, c) => r.concat(JSON.parse(c)), []);
+      allFlattenCommits.map((branchCommits) => {
+        console.log(111111111);
+        let branchCommitArray = JSON.parse(branchCommits);
+        // Filter duplicates
+        branchCommitArray.sort((lhs, rhs) => {
+          return Date.parse(lhs.committer.date) - Date.parse(rhs.committer.date);
+        });
         
+        const { accum : uniqCommitArray } = branchCommitArray.reduce((container, branchCommit)=>{
+          if (container.lookup[branchCommit.sha] === undefined) {
+            container.lookup[branchCommit.sha] = true;
+            container.accum.push(branchCommit);
+           }
+           return container;
+         }, requestAllCommits);
       });
+
       res.status(200).end('youooo');
+      // res.status(200).json("hey");
+    }, (reason) => {
+      // console.log(7777, reason);
+      res.status(401).end('noooooo');
     });
     
     // functions
     function makeRequestBranchCommits(results, branch, index) {
-      const commitsOpt = { uri : `${commitsURL}?sha=${branch.commit.sha}&${secrets}`, headers : { 'User-Agent' : userAgent } };
+      const commitsOpt = { uri : `${commitsURL}?sha=${branch.commit.sha}&${secrets}`,
+                           headers : { 'User-Agent' : userAgent } };
       const branchPromise = request(commitsOpt);
       results.push(branchPromise);
       return results;
@@ -55,18 +78,6 @@ router.route('/repos/:userName/:repoName')
     
   })
 
-
-  /*
-  request({ url : address, headers : { 'User-Agent' : userAgent } },
-          function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.status(200).json(body);
-    } else {
-      console.log("invalid", address);
-      res.end();
-    }
-  });
-   */
 });
 
 
