19261d1a9ac32e27367d705936549a0b87684c5f
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-27 09:45:02 -0700

    (task) Remove babel-regenerator-runtime from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
 module.exports = {
   entry: [
-    'babel-regenerator-runtime',
-    './src/index.js',
+    path.join(__dirname, '/src/index.js'),
   ],
   devtool: 'source-map',
   output: {
