0761d4583f08e9ecb6c0b9be72adc511180f3e26
Timothy Shiu <timoweave@gmail.com>
2016-09-29 14:46:18 -0700

    (task) move server files into server directory

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
-const webpackConfig = require('./webpack.config');
-const api = require('./api');
+const webpackConfig = require('../webpack.config');
+const githubAPI = require('./api');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -23,14 +24,17 @@ app.use(webpackMiddleware(compiler, {
   publicPath: webpackConfig.output.publicPath,
 }));
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname, '/..')));
 
-app.use('/api', api);
+app.use('/api', githubAPI);
 
-app.get('*', function (req, res) {
-  console.log('Serving /');
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  const file = path.join(__dirname, 'index.html');
+  console.log('Serving /', file);
+  res.sendFile(file);
 });
 
-app.listen(port);
-console.log('Listening on port ', port);
+app.listen(port, function() {
+  console.log('Listening on port ', port);
+});
+
